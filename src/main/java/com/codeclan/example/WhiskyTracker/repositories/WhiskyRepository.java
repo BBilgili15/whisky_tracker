6f9c523ef272521858f6c4cad3b87a9deb8f215d
Ben Bilgili <benbilgili@gmail.com>
2023-07-06 09:05:53 +0100

    initial

---
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository.java
@@ -0,0 +1,19 @@
+package com.codeclan.example.WhiskyTracker.repositories;
+
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface WhiskyRepository extends JpaRepository<Whisky, Long> {
+    List<Whisky> findByAge(int age);
+
+    List<Whisky> findByDistillery_Region(String region);
+
+    List<Whisky> findByAgeAndDistillery_Id(int age, Long id);
+
+
+
+
+
+}
---
+++ src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
@@ -0,0 +1,49 @@
+package com.codeclan.example.WhiskyTracker;
+
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test") //Indicates it's a test profile so will not run DataLoader
+@SpringBootTest
+public class WhiskyTrackerApplicationTests {
+
+	@Autowired
+	private WhiskyRepository whiskyRepository;
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void canFindWhiskyByAge() {
+		List<Whisky> foundWhiskies = whiskyRepository.findByAge(12);
+		assertEquals(12, foundWhiskies.size());
+	}
+
+	@Test
+	public void canFindWhiskyByRegion() {
+		List<Whisky> foundWhiskies = whiskyRepository.findByDistillery_Region("Speyside");
+		assertEquals(9, foundWhiskies.size());
+	}
+
+	@Test
+	public void canFindWhiskyByAgeAndDistillery() {
+		List<Whisky> foundWhiskies = whiskyRepository.findByAgeAndDistillery_Id(12, 1L);
+		assertEquals(1, foundWhiskies.size());
+	}
+
+	
+
+}
